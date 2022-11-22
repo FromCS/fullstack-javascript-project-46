a79b11ba80ceb4942ff83afd86979d004dd8682b
Mikhail Gostjukhin <mkg0st@yandex.ru>
2022-11-22 22:04:45 +0300

    Refactor gendiff function and add secondary functions.
    Change const 'difference' from array to object.
    Add stringify function for create needed string from object.
    Add secondary function getDiff for making object of differences.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,51 @@
 import _ from 'lodash';
-import { cwd } from 'process';
 import path from 'path';
 import { readFileSync } from 'fs';
 
-const getAbsolutePath = (filepath) => path.resolve(cwd(), filepath);
+const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
 
-const getDataFromPath = (filepath) => {
+const readFile = (filepath) => {
   const absolutePath = getAbsolutePath(filepath);
   return JSON.parse(readFileSync(absolutePath));
 };
 
-const genDiff = (filepath1, filepath2) => {
-  const data1 = getDataFromPath(filepath1);
-  const data2 = getDataFromPath(filepath2);
+const stringify = (obj, replacer = ' ', spaceCount = 2) => {
+  const entries = Object.entries(obj);
+  const string = entries.map(([key, value]) => `${replacer.repeat(spaceCount)}${key}: ${value}`);
+  return `{\n${string.join('\n')}\n}`;
+};
+
+const getDiff = (data1, data2) => {
   const sortedGeneralKeys = _.sortBy(Object.keys({ ...data1, ...data2 }));
-  const difference = sortedGeneralKeys.map((key) => {
-    if (_.has(data1, key) && _.has(data2, key)) {
-      if (data1[key] === data2[key]) {
-        return `    ${key}: ${data1[key]}`;
-      }
-      return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
-    }
+  const difference = sortedGeneralKeys.reduce((acc, key) => {
     if (!_.has(data1, key)) {
-      return `  + ${key}: ${data2[key]}`;
+      acc[`+ ${key}`] = data2[key];
+      return acc;
     }
 
-    return `  - ${key}: ${data1[key]}`;
-  });
-  console.log(`{\n${difference.join('\n')}\n}`);
+    if (!_.has(data2, key)) {
+      acc[`- ${key}`] = data1[key];
+      return acc;
+    }
+
+    if (data1[key] === data2[key]) {
+      acc[`  ${key}`] = data1[key];
+      return acc;
+    }
+    acc[`- ${key}`] = data1[key];
+    acc[`+ ${key}`] = data2[key];
+    return acc;
+  }, {});
+
+  return difference;
+};
+
+const genDiff = (filepath1, filepath2) => {
+  const data1 = readFile(filepath1);
+  const data2 = readFile(filepath2);
+  const difference = getDiff(data1, data2);
+  console.log(stringify(difference));
+  return stringify(difference);
 };
 
 export default genDiff;
