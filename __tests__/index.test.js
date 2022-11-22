c39b1859e50a92b3f913894d2e2272a5f48b58e1
Mikhail Gostjukhin <mkg0st@yandex.ru>
2022-11-22 22:12:10 +0300

    add test file for testing flat differences

---
+++ __tests__/index.test.js
@@ -0,0 +1,13 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { readFileSync } from 'fs';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
+test('gendiff', () => {
+  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toBe(readFile('expected_file.txt'));
+});
