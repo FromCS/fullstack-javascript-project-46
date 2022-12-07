243b2de178f7358456e76c6e81a90d4ffc70e720
Mikhail Gostjukhin <mkg0st@yandex.ru>
2022-12-07 22:51:58 +0300

    add default in option "format", refactor action with genDiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,9 +8,11 @@ const program = new Command();
 program
   .description('Compare two configuration files and shows a difference')
   .version('1.0.0')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .action(genDiff);
+  .action((filepath1, filepath2, format = program.opts().format) => {
+    genDiff(filepath1, filepath2, format.format);
+  });
 
 program.parse(process.argv);
