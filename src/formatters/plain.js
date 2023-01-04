cd035b23297eb736bf6b11a819c4801502d9e0d1
Mikhail Gostjukhin <mkg0st@yandex.ru>
2023-01-04 20:16:37 +0300

    remove code of makePlain function

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,21 +1,5 @@
 import _ from 'lodash';
 
-const makePlain = (obj) => {
-  const iter = (currentValue, acc) => {
-    
-    const entries = Object.entries(currentValue);
-    const result = entries.map(([key, value], i, arr) => {
-      const 
-      if (arr[i + 1]) {
-
-      }
-      
-      if (key.startsWith('+') || key.startsWith('-')) {
-        
-      }
-    });
-  };
-  iter(obj, '');
-};
+const makePlain = (obj) => 'to be continious';
 
 export default makePlain;
