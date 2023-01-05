28eeed9ebdb16d5c427983e1a8dc98ca50bc6191
Mikhail Gostjukhin <mkg0st@yandex.ru>
2023-01-05 17:52:37 +0300

    refactor genDiff function

--- src/index.js
+++ src/index.js
@@ -35,26 +35,25 @@ const makeDiff = (key, values, data, acc) => {
   return acc;
 };
 
-const getDiff = (data1, data2) => {
-  const sortedGeneralKeys = getSortedKeysByName(data1, data2);
-  const difference = sortedGeneralKeys.reduce((acc, key) => {
-    const value1 = data1[key];
-    const value2 = data2[key];
-    if (_.isObject(value1) && _.isObject(value2)) {
-      acc[key] = getDiff(value1, value2);
-      return acc;
-    }
-    return makeDiff(key, [value1, value2], [data1, data2], acc);
-  }, {});
-  return difference;
-};
-
 const genDiff = (filepath1, filepath2, formatter = 'stylish') => {
   const data1 = readFile(filepath1, getExtension(filepath1));
   const data2 = readFile(filepath2, getExtension(filepath2));
-  const difference = getDiff(data1, data2);
-  console.log(stringify(difference, formatter));
-  return stringify(difference, formatter);
+  const iter = (currentData1, currentData2) => {
+    const sortedGeneralKeys = getSortedKeysByName(currentData1, currentData2);
+    const difference = sortedGeneralKeys.reduce((acc, key) => {
+      const value1 = currentData1[key];
+      const value2 = currentData2[key];
+      if (_.isObject(value1) && _.isObject(value2)) {
+        acc[key] = iter(value1, value2);
+        return acc;
+      }
+      return makeDiff(key, [value1, value2], [currentData1, currentData2], acc);
+    }, {});
+    return difference;
+  };
+
+  console.log(stringify(iter(data1, data2), formatter));
+  return stringify(iter(data1, data2), formatter);
 };
 
 export default genDiff;
