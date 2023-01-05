4e801b5652a3a90b2d71777bedeec2f4093abc92
Mikhail Gostjukhin <mkg0st@yandex.ru>
2023-01-05 18:55:00 +0300

    Rename & add functions in plain.js module.
    Rename getStringToPlainFormatter to comparePropertyInOneData.
    Rename genPlainDiff to comparePropertyInBothData.
    Add definePointInComplexProperty function for creating (or not) point in property ACC.

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -4,12 +4,9 @@ const isChanged = (key) => key.startsWith('+') || key.startsWith('-');
 
 const makeQuotes = (value) => ((typeof value === 'boolean' || value === null) ? value : `'${value}'`);
 
-// const getStringOfPlainFormatter = (currentKey, value, newKey) => {
-// if ()
-// };
 const defineComplexValue = (value) => (_.isObject(value) ? '[complex value]' : makeQuotes(value));
 
-const getStringOfPlainFormatter = (currentKey, value, newKey) => {
+const comparePropertyInOneData = (currentKey, value, newKey) => {
   switch (currentKey[0]) {
     case '+':
       return `Property '${newKey}' was added with value: ${defineComplexValue(value)}`;
@@ -20,14 +17,16 @@ const getStringOfPlainFormatter = (currentKey, value, newKey) => {
   }
 };
 
-const genPlainDiff = (entries, newKey, nextEntries, previousKey) => {
+const definePointInComplexProperty = ((value) => (value === '' ? '' : '.'));
+
+const comparePropertyInBothData = (entries, newKey, nextEntries, previousKey) => {
   const [currentKey, currentValue] = entries;
   const [nextKey, nextValue] = nextEntries;
   if (`+ ${currentKey.slice(2)}` === nextKey) {
     return `Property '${newKey}' was updated. From ${defineComplexValue(currentValue)} to ${makeQuotes(nextValue)}`;
   }
   if (`- ${currentKey.slice(2)}` !== previousKey) {
-    return getStringOfPlainFormatter(currentKey, currentValue, newKey);
+    return comparePropertyInOneData(currentKey, currentValue, newKey);
   }
 };
 
@@ -36,12 +35,12 @@ const makePlain = (obj) => {
     const entries = Object.entries(currentValue);
     const plain = entries.map(([key, value], i, arr) => {
       if (!isChanged(key) && _.isObject(value)) {
-        return iter(value, `${acc}${acc === '' ? '' : '.'}${key}`);
+        return iter(value, `${acc}${definePointInComplexProperty(acc)}${key}`);
       }
-      const newKey = `${acc}${acc === '' ? '' : '.'}${key.slice(2)}`;
+      const newKey = `${acc}${definePointInComplexProperty(acc)}${key.slice(2)}`;
       const [nextKey, nextValue] = arr[i + 1] === undefined ? '' : arr[i + 1];
       const [previousKey] = arr[i - 1] === undefined ? '' : arr[i - 1];
-      return genPlainDiff([key, value], newKey, [nextKey, nextValue], previousKey);
+      return comparePropertyInBothData([key, value], newKey, [nextKey, nextValue], previousKey);
     });
 
     return _.compact(plain).join('\n');
