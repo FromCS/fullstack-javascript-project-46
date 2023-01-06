fe6995dede2688f2e33076a0b519bcdccccc8cb0
Mikhail Gostjukhin <mkg0st@yandex.ru>
2023-01-06 22:04:39 +0300

    create json.js file which contain empty makeJson function

---
+++ src/formatters/json.js
@@ -0,0 +1,11 @@
+import _ from 'lodash';
+
+const makeJson = (obj) => {
+  const iter = (currentData) => {
+    return 'to be continious';
+  };
+
+  return iter(obj);
+};
+
+export default makeJson;
