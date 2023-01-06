f7fab331854b11f63f3d6447d02baadf307dd54a
Mikhail Gostjukhin <mkg0st@yandex.ru>
2023-01-06 22:00:18 +0300

    Prepare tests of JSON formatter.
    Create expected_file_json.txt which contain needed result;
    Add test 'gendiff json' in index.test.js.

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,11 +9,16 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 test('gendiff stylish', () => {
-  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toBe(readFile('expected_file.txt'));
-  expect(genDiff('./__fixtures__/file1.yml', './__fixtures__/file2.yaml')).toBe(readFile('expected_file.txt'));
+  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toBe(readFile('expected_file_stylish.txt'));
+  expect(genDiff('./__fixtures__/file1.yml', './__fixtures__/file2.yaml')).toBe(readFile('expected_file_stylish.txt'));
 });
 
 test('gendiff plain', () => {
   expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json', 'plain')).toBe(readFile('expected_file_plain.txt'));
   expect(genDiff('./__fixtures__/file1.yml', './__fixtures__/file2.yaml', 'plain')).toBe(readFile('expected_file_plain.txt'));
+});
+
+test('gendiff json', () => {
+  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json', 'json')).toBe(readFile('expected_file_json.txt'));
+  expect(genDiff('./__fixtures__/file1.yml', './__fixtures__/file2.yaml', 'json')).toBe(readFile('expected_file_json.txt'));
 });
