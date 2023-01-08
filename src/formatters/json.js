d622505c913519747fcb8a344cd9a14a04183551
Mikhail Gostjukhin <mkg0st@yandex.ru>
2023-01-08 19:50:50 +0300

    realize makeJson function on src/formatters/json.js module

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,8 +1,18 @@
 import _ from 'lodash';
 
+const makeDoubleQuotes = (value) => ((typeof value === 'string')
+  ? `"${value}"`
+  : value);
+
 const makeJson = (obj) => {
-  const iter = (currentData) => {
-    return 'to be continious';
+  const iter = (currentValue) => {
+    const entries = Object.entries(currentValue);
+
+    const json = entries.map(([key, value]) => (_.isObject(value)
+      ? [`"${key}":${iter(value)}`]
+      : [`"${key}":${makeDoubleQuotes(value)}`]));
+
+    return `[{${json.join(',')}}]`;
   };
 
   return iter(obj);
