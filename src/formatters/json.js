31867ee09a0f8de3f2800996c9f2370d8ecd3bb0
Mikhail Gostjukhin <mkg0st@yandex.ru>
2023-01-16 21:54:30 +0300

    refactor makeJson function: change iterative process to recursion

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -5,15 +5,11 @@ const makeDoubleQuotes = (value) => ((typeof value === 'string')
   : value);
 
 const makeJson = (diffs) => {
-  const iter = (currentValue) => {
-    const json = currentValue.map(([key, value]) => (_.isObject(value)
-      ? [`"${key}":${iter(value)}`]
-      : [`"${key}":${makeDoubleQuotes(value)}`]));
+  const json = diffs.map(([key, value]) => (_.isObject(value)
+    ? [`"${key}":${makeJson(value)}`]
+    : [`"${key}":${makeDoubleQuotes(value)}`]));
 
-    return `[{${json.join(',')}}]`;
-  };
-
-  return iter(diffs);
+  return `[{${json.join(',')}}]`;
 };
 
 export default makeJson;
