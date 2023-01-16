580ce72bdb0ed9bc6661cbed49131f9864a1a070
Mikhail Gostjukhin <mkg0st@yandex.ru>
2023-01-16 21:47:44 +0300

    Refactor src/index.js module.
    Remove makeDiff function and move this in src/makeDiff.js module.
    Refactor genDiff function. Get raw information in it and giving to getParsedContent function.
    Remove getSortedKeys function and move functional of this to const sortedGeneralKeysByName.

--- src/index.js
+++ src/index.js
@@ -1,46 +1,22 @@
 import _ from 'lodash';
 import path from 'path';
-import readFile from './parsers.js';
+import { readFileSync } from 'fs';
+import getParsedContent from './parsers.js';
 import stringify from './formatters/index.js';
-
-const getArrFromObj = (obj) => {
-  if (_.isObject(obj)) {
-    const entries = Object.entries(obj);
-    const array = entries.map(([key, value]) => (_.isObject(value)
-      ? [key, getArrFromObj(value)]
-      : [key, value]));
-    return array;
-  }
-  return obj;
-};
+import makeDiff from './makeDiff.js';
 
 const getExtension = (filepath) => path.extname(filepath);
 
-const getSortedKeysByName = (data1, data2) => _.sortBy(Object.keys({ ...data1, ...data2 }));
-
-const makeDiff = (key, values, data) => {
-  const [value1, value2] = values;
-  const [data1, data2] = data;
-  if (!_.has(data1, key)) {
-    return [[`+ ${key}`, getArrFromObj(value2)]];
-  }
-
-  if (!_.has(data2, key)) {
-    return [[`- ${key}`, getArrFromObj(value1)]];
-  }
-
-  if (value1 === value2) {
-    return [[key, getArrFromObj(value1)]];
-  }
-  return [[`- ${key}`, getArrFromObj(value1)], [`+ ${key}`, getArrFromObj(value2)]];
-};
+const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
 
 const genDiff = (filepath1, filepath2, formatter = 'stylish') => {
-  const data1 = readFile(filepath1, getExtension(filepath1));
-  const data2 = readFile(filepath2, getExtension(filepath2));
+  const absolut1 = getAbsolutePath(filepath1);
+  const absolut2 = getAbsolutePath(filepath2);
+  const data1 = getParsedContent(readFileSync(absolut1), getExtension(filepath1));
+  const data2 = getParsedContent(readFileSync(absolut2), getExtension(filepath2));
   const iter = (currentData1, currentData2) => {
-    const sortedGeneralKeys = getSortedKeysByName(currentData1, currentData2);
-    const difference = sortedGeneralKeys.map((key) => {
+    const sortedGeneralKeysByName = _.sortBy(Object.keys({ ...currentData1, ...currentData2 }));
+    const difference = sortedGeneralKeysByName.map((key) => {
       const value1 = currentData1[key];
       const value2 = currentData2[key];
       if (_.isObject(value1) && _.isObject(value2)) {
