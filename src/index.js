5ac2b5d57714fcb59c71be969008b0866930fd71
Mikhail Gostjukhin <mkg0st@yandex.ru>
2023-01-23 14:09:14 +0300

    refactor gendiff function: change structure of tree of diffs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import path from 'path';
 import { readFileSync } from 'fs';
 import getParsedContent from './parsers.js';
@@ -14,21 +13,8 @@ const genDiff = (filepath1, filepath2, formatter = 'stylish') => {
   const absolut2 = getAbsolutePath(filepath2);
   const data1 = getParsedContent(readFileSync(absolut1), getExtension(filepath1));
   const data2 = getParsedContent(readFileSync(absolut2), getExtension(filepath2));
-  const iter = (currentData1, currentData2) => {
-    const sortedGeneralKeysByName = _.sortBy(Object.keys({ ...currentData1, ...currentData2 }));
-    const difference = sortedGeneralKeysByName.map((key) => {
-      const value1 = currentData1[key];
-      const value2 = currentData2[key];
-      if (_.isObject(value1) && _.isObject(value2)) {
-        return [[key, iter(value1, value2)]];
-      }
-      return makeDiff(key, [value1, value2], [currentData1, currentData2]);
-    });
-    return _.flatten(difference);
-  };
-
-  console.log(stringify(iter(data1, data2), formatter));
-  return stringify(iter(data1, data2), formatter);
+  console.log(stringify(makeDiff(data1, data2), formatter));
+  return stringify(makeDiff(data1, data2), formatter);
 };
 
 export default genDiff;
