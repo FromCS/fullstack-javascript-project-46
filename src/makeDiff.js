252f37a79005c7c35a19607316d8042cb47e76c5
Mikhail Gostjukhin <mkg0st@yandex.ru>
2023-01-23 15:13:45 +0300

    refactor makeDiff function: add new function compareObjects which return object of comparing

--- src/makeDiff.js
+++ src/makeDiff.js
@@ -1,5 +1,25 @@
 import _ from 'lodash';
 
+const compareObjects = (key, data, values) => {
+  const [value1, value2] = values;
+  const [data1, data2] = data;
+  if (!_.has(data1, key)) {
+    return { key, status: 'added', value: value2 };
+  }
+
+  if (!_.has(data2, key)) {
+    return { key, status: 'deleted', value: value1 };
+  }
+
+  if (value1 !== value2) {
+    return {
+      key, status: 'changed', oldValue: value1, newValue: value2,
+    };
+  }
+
+  return { key, status: 'unchanged', value: value1 };
+};
+
 const makeDiff = (data1, data2) => {
   const sortedKeysByName = _.sortBy(Object.keys({ ...data1, ...data2 }));
   const treeOfDiff = sortedKeysByName.map((key) => {
@@ -9,22 +29,7 @@ const makeDiff = (data1, data2) => {
     if (_.isObject(value1) && _.isObject(value2)) {
       return { key, status: 'nested', children: makeDiff(value1, value2) };
     }
-
-    if (!_.has(data1, key)) {
-      return { key, status: 'added', value: value2 };
-    }
-
-    if (!_.has(data2, key)) {
-      return { key, status: 'deleted', value: value1 };
-    }
-
-    if (value1 !== value2) {
-      return {
-        key, status: 'changed', oldValue: value1, newValue: value2,
-      };
-    }
-
-    return { key, status: 'unchanged', value: value1 };
+    return compareObjects(key, [data1, data2], [value1, value2]);
   });
 
   return treeOfDiff;
