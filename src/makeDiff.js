1ff10eb9f5d3016fa30da1007825bacbd7dea1e7
Mikhail Gostjukhin <mkg0st@yandex.ru>
2023-01-23 14:11:13 +0300

    refactor makeDiff function: change structure of final tree of diffs, remove unnecessary functions from module

--- src/makeDiff.js
+++ src/makeDiff.js
@@ -1,31 +1,33 @@
 import _ from 'lodash';
 
-const getArrFromObj = (obj) => {
-  if (_.isObject(obj)) {
-    const entries = Object.entries(obj);
-    const array = entries.map(([key, value]) => (_.isObject(value)
-      ? [key, getArrFromObj(value)]
-      : [key, value]));
-    return array;
-  }
-  return obj;
-};
+const makeDiff = (data1, data2) => {
+  const sortedKeysByName = _.sortBy(Object.keys({ ...data1, ...data2 }));
+  const treeOfDiff = sortedKeysByName.map((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
 
-const makeDiff = (key, values, data) => {
-  const [value1, value2] = values;
-  const [data1, data2] = data;
-  if (!_.has(data1, key)) {
-    return [[`+ ${key}`, getArrFromObj(value2)]];
-  }
+    if (_.isObject(value1) && _.isObject(value2)) {
+      return { key, status: 'nested', children: makeDiff(value1, value2) };
+    }
 
-  if (!_.has(data2, key)) {
-    return [[`- ${key}`, getArrFromObj(value1)]];
-  }
+    if (!_.has(data1, key)) {
+      return { key, status: 'added', value: value2 };
+    }
 
-  if (value1 === value2) {
-    return [[key, getArrFromObj(value1)]];
-  }
-  return [[`- ${key}`, getArrFromObj(value1)], [`+ ${key}`, getArrFromObj(value2)]];
+    if (!_.has(data2, key)) {
+      return { key, status: 'deleted', value: value1 };
+    }
+
+    if (value1 !== value2) {
+      return {
+        key, status: 'changed', oldValue: value1, newValue: value2,
+      };
+    }
+
+    return { key, status: 'unchanged', value: value1 };
+  });
+
+  return treeOfDiff;
 };
 
 export default makeDiff;
