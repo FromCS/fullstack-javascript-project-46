ba15bcb480d374c49a8188eae802d54b72afc21a
Mikhail Gostjukhin <mkg0st@yandex.ru>
2023-01-29 22:33:46 +0300

    remove compareObjects function and move code of this function to makeDiff function

--- src/makeDiff.js
+++ src/makeDiff.js
@@ -1,27 +1,7 @@
 import _ from 'lodash';
 
-const compareObjects = (key, data, values) => {
-  const [value1, value2] = values;
-  const [data1, data2] = data;
-  if (!_.has(data1, key)) {
-    return { key, status: 'added', value: value2 };
-  }
-
-  if (!_.has(data2, key)) {
-    return { key, status: 'deleted', value: value1 };
-  }
-
-  if (value1 !== value2) {
-    return {
-      key, status: 'changed', oldValue: value1, newValue: value2,
-    };
-  }
-
-  return { key, status: 'unchanged', value: value1 };
-};
-
 const makeDiff = (data1, data2) => {
-  const sortedKeysByName = _.sortBy(Object.keys({ ...data1, ...data2 }));
+  const sortedKeysByName = _.sortBy(_.union(Object.keys({ ...data1, ...data2 })));
   const treeOfDiff = sortedKeysByName.map((key) => {
     const value1 = data1[key];
     const value2 = data2[key];
@@ -29,7 +9,20 @@ const makeDiff = (data1, data2) => {
     if (_.isObject(value1) && _.isObject(value2)) {
       return { key, status: 'nested', children: makeDiff(value1, value2) };
     }
-    return compareObjects(key, [data1, data2], [value1, value2]);
+    if (!_.has(data1, key)) {
+      return { key, status: 'added', value: value2 };
+    }
+
+    if (!_.has(data2, key)) {
+      return { key, status: 'deleted', value: value1 };
+    }
+
+    if (value1 !== value2) {
+      return {
+        key, status: 'changed', oldValue: value1, newValue: value2,
+      };
+    }
+    return { key, status: 'unchanged', value: value1 };
   });
 
   return treeOfDiff;
