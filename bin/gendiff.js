6df580150c8f3a7fa5bf6d0725e7b79482ac87ad
Mikhail Gostjukhin <mkg0st@yandex.ru>
2023-01-29 22:29:19 +0300

    Refactor genDiff function in src/index.js module.
    Move console.log statement with result of gendiff function from src/index.js to bin/gendiff/js.
    Rename const absolut1 and absolut2 to firstFileAbsolutePath and secondFileAbsolutePath.
    Rename stringify funtion to getFormattedContent in src/index.js module.

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -12,7 +12,7 @@ program
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2, format = program.opts().format) => {
-    genDiff(filepath1, filepath2, format.format);
+    console.log(genDiff(filepath1, filepath2, format.format));
   });
 
 program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import { readFileSync } from 'fs';
 import getParsedContent from './parsers.js';
-import stringify from './formatters/index.js';
+import getFormattedContent from './formatters/index.js';
 import makeDiff from './makeDiff.js';
 
 const getExtension = (filepath) => path.extname(filepath);
@@ -9,12 +9,11 @@ const getExtension = (filepath) => path.extname(filepath);
 const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
 
 const genDiff = (filepath1, filepath2, formatter = 'stylish') => {
-  const absolut1 = getAbsolutePath(filepath1);
-  const absolut2 = getAbsolutePath(filepath2);
-  const data1 = getParsedContent(readFileSync(absolut1), getExtension(filepath1));
-  const data2 = getParsedContent(readFileSync(absolut2), getExtension(filepath2));
-  console.log(stringify(makeDiff(data1, data2), formatter));
-  return stringify(makeDiff(data1, data2), formatter);
+  const firstFileAbsolutePath = getAbsolutePath(filepath1);
+  const secondFileAbsolutePath = getAbsolutePath(filepath2);
+  const data1 = getParsedContent(readFileSync(firstFileAbsolutePath), getExtension(filepath1));
+  const data2 = getParsedContent(readFileSync(secondFileAbsolutePath), getExtension(filepath2));
+  return getFormattedContent(makeDiff(data1, data2), formatter);
 };
 
 export default genDiff;
