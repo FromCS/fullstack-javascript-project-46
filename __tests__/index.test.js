a41c6b240649411fd27d3fe8f80adaf5e7123b97
Mikhail Gostjukhin <mkg0st@yandex.ru>
2023-01-29 22:32:41 +0300

    improve tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,21 +5,18 @@ import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const extensions = ['json', 'yml'];
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+const stylishResult = readFile('expected_file_stylish.txt');
+const plainResult = readFile('expected_file_plain.txt');
+const jsonResult = readFile('expected_file_json.txt');
 
-test('gendiff stylish', () => {
-  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toBe(readFile('expected_file_stylish.txt'));
-  expect(genDiff('./__fixtures__/file1.yml', './__fixtures__/file2.yaml')).toBe(readFile('expected_file_stylish.txt'));
-  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json', 'stylish')).toBe(readFile('expected_file_stylish.txt'));
-});
-
-test('gendiff plain', () => {
-  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json', 'plain')).toBe(readFile('expected_file_plain.txt'));
-  expect(genDiff('./__fixtures__/file1.yml', './__fixtures__/file2.yaml', 'plain')).toBe(readFile('expected_file_plain.txt'));
-});
-
-test('gendiff json', () => {
-  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json', 'json')).toBe(readFile('expected_file_json.txt'));
-  expect(genDiff('./__fixtures__/file1.yml', './__fixtures__/file2.yaml', 'json')).toBe(readFile('expected_file_json.txt'));
+test.each(extensions)('gendiff %s', (extension) => {
+  const fileBefore = `./__fixtures__/file1.${extension}`;
+  const fileAfter = `./__fixtures__/file2.${extension}`;
+  expect(genDiff(fileBefore, fileAfter)).toBe(stylishResult);
+  expect(genDiff(fileBefore, fileAfter, 'stylish')).toBe(stylishResult);
+  expect(genDiff(fileBefore, fileAfter, 'plain')).toBe(plainResult);
+  expect(genDiff(fileBefore, fileAfter, 'json')).toBe(jsonResult);
 });
